6cd69ad2c4172eefab7af889f66db08e0aef8b85
hortune <b04611015@ntu.edu.tw>
2017-02-23 01:05:51 -0800

    fix multi

--- hw0/one.py
+++ hw0/one.py
@@ -3,6 +3,8 @@ from numpy import genfromtxt,matmul
 matrix1 = genfromtxt(sys.argv[1],delimiter=',')
 matrix2 = genfromtxt(sys.argv[2],delimiter=',')
 ls = matmul(matrix1,matrix2)
+if type(ls[0]) == type(matrix1):
+    ls = [j for i in ls for j in i]
 ls.sort()
 with open("ans_one.txt","w+") as fd:
     for i in ls:
