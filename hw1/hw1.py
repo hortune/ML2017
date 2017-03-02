df70b1d5c102c4aac75c90b9b116f338f640f425
hortune <b04611015@ntu.edu.tw>
2017-03-02 08:24:25 -0800

    my code fucking doesnt work

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,15 +1,40 @@
 # coding: utf-8
 import numpy as np
 from numpy import *
+import math
+class Regression(object):
+    def __init__ (self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+    def fit(self,x,y):
+        self.w_ = np.zeros(1 + 18*9) # make bias the zero one
+        for i in range(0,self.n_iter):
+            output = np.dot(x,self.w_[1:])+self.w_[0]
+            errors = y- output 
+            if i<5:
+                print errors
+            self.w_[1:]+=self.eta*x.T.dot(errors)
+            self.w_[0]+=self.eta*errors.sum()
+        return self
+    def activation(self,x):
+        return np.dot(x,self.w_[1:])+self.w_[0]
 
-"""
-manipulate training data x,y
-"""
 data=genfromtxt('train.csv',delimiter=',')
+for i in range(0,data.shape[0]):
+    for j in range(0,data.shape[1]):
+        if math.isnan(data[i][j]):
+            data[i][j]=0
 data=np.delete(np.delete(data,0,0),np.s_[0:3],1)
 data=np.delete(data,np.s_[10:],1)
 data=np.split(data,240)
-x=[ i[9][0:9] for i in data]
-y=[ i[9][9] for i in data]
 
-
+y=np.array([ i[9][9] for i in data])
+#x=np.array([ i[9][0:9] for i in data])
+data=np.delete(data,np.s_[9:],2)
+x=np.array([ np.ravel(i) for i in data])
+print len(x[0])
+k=Regression(0.01,50).fit(x,y)
+total_data=240
+total_true=0
+#for (a,b) in zip(x,y):
+#    print k.activation(a),b
