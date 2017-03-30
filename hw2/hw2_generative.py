ae6bb0511a3679555f5ce07d4339865f472260db
hortune <b04611015@ntu.edu.tw>
2017-03-29 20:56:30 -0700

    some bug

--- hw2/hw2_generative.py
+++ hw2/hw2_generative.py
@@ -10,8 +10,11 @@ class Generative(object):
     def guassian_mean_sigma(self,c1):
         mean = c1.sum(axis=0,dtype=np.float64)/c1.shape[0]
         x_minus_mean = c1-mean
-        x_minus_mean = x_minus_mean.sum(axis=0,dtype=np.float64)/c1.shape[0]
-        sigma = np.outer(x_minus_mean,x_minus_mean)
+        #x_minus_mean = x_minus_mean.sum(axis=0,dtype=np.float64)/c1.shape[0]
+        sigma = 0
+        for row in x_minus_mean:
+            sigma += np.outer(row,row)
+        sigma /= c1.shape[0]
         return mean,sigma
     
     def guassian_model(self,c1,c2):
@@ -21,6 +24,7 @@ class Generative(object):
         self.rate = c1.shape[0]/(c2.shape[0]+c1.shape[0])
         self.sigma = true_sigma*self.rate + (1-self.rate)*false_sigma
         self.inverse = np.linalg.inv(self.sigma)
+    
         return self       
 
     def activate(self,x):
@@ -31,32 +35,47 @@ class Generative(object):
         false_numerator = -(false_x.dot(self.inverse)*true_x).sum(axis=1)/2
         
         numerator = false_numerator - true_numerator
-        print(numerator)
-        #ans = 1/(1+np.exp(numerator)*self.rate/(1-self.rate))
-        #print(ans[0])
+        ans = 1/(1+np.exp(numerator)*self.rate/(1-self.rate))
+        return ans
+    def new_activate(self,x):
+        w = (self.true_mean-self.false_mean).dot(self.inverse).dot(x.T)
+        b1 = -(self.true_mean).dot(self.inverse).dot(self.true_mean.T)/2
+        b2 = (self.false_mean).dot(self.inverse).dot(self.false_mean.T)/2
+        b3 = math.log((self.rate)/(1-self.rate))
+        ans = 1/(1+np.exp(-w+b1-b2-b3))
+        return ans
+    def validate(self,x,y):
+        ans = self.new_activate(x)
+        error = 0
+        for i,j in zip(ans,y):
+            if round(i,0) != j:
+                error += 1
+        return error/x.shape[0]
+                
+
 def load_training_data(filename,Y_filename):
     x = genfromtxt(filename,delimiter=',',dtype=np.float64)
     y = genfromtxt(Y_filename,delimiter=',')
     x = np.delete(x,0,0)
     
-    """
-    normalize prevent underflow
-    """
+    #x = np.hstack((x,x[:,0].reshape((x.shape[0],1))**3/1e-11))
+    x = np.hstack((x,x[:,0:6]**2/1e-5))
+    x = np.hstack((x,x[:,0:6]**3/1e-11))
+    #x = np.hstack((x,x[:,0:6]**4/1e-20))
     mean = x.mean(axis=0,dtype=np.float64)
     x -= mean
     sigma = (x**2).mean(axis=0,dtype=np.float64)/x.shape[0]
     x /= sigma
     
-    
     true_data=[]
     false_data=[]
-
+    
     for q,p in zip(x,y):
         if p == 0:
             false_data.append(q)
         else:
             true_data.append(q)
-    return np.array(true_data),np.array(false_data),mean,sigma
+    return x,y,np.array(true_data),np.array(false_data),mean,sigma
 
 def load_testing_data(filename,normal,s1,s2):
     x = genfromtxt(filename,delimiter=',')
@@ -65,9 +84,11 @@ def load_testing_data(filename,normal,s1,s2):
     return x
     
 if __name__=='__main__':
-    true_data,false_data,mean,sigma = load_training_data("X_train","Y_train")
+    x,y,true_data,false_data,mean,sigma = load_training_data("X_train","Y_train")
+    #true_data = np.array([[1,0],[0,1]])
+    #false_data = np.array([[1,2],[3,4]])
     k = Generative().guassian_model(true_data,false_data)
-    k.activate(true_data)
+    print(k.validate(x,y))
     #test = load_testing_data("X_test",normal,0,6)
     """
     with open("submission.csv","w+") as fd:
