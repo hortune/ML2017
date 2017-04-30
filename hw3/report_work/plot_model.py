fd3b3b19ca2310c326f39dce63ceeb0883b20863
hortune <b04611015@ntu.edu.tw>
2017-04-30 05:38:46 -0700

    plot_model

---
+++ hw3/report_work/plot_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+import os
+from termcolor import colored, cprint
+import argparse
+from keras.utils.visualize_util import plot
+from keras.models import load_model
+
+base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#exp_dir = os.path.join(base_dir,'exp')
+
+def main():
+    """
+    parser = argparse.ArgumentParser(prog='plot_model.py',
+            description='Plot the model.')
+    parser.add_argument('--model',type=str,default='simple',choices=['simple','easy','strong'],
+            metavar='<model>')
+    parser.add_argument('--epoch',type=int,metavar='<#epoch>',default=80)
+    parser.add_argument('--batch',type=int,metavar='<batch_size>',default=64)
+    parser.add_argument('--idx',type=int,metavar='<suffix>',required=True)
+    args = parser.parse_args()
+    """
+    print(colored("Loading model from {}".format('../'),'yellow',attrs=['bold']))
+    
+    emotion_classifier = load_model('../last_model')
+    emotion_classifier.summary()
+    plot(emotion_classifier,to_file='model.png')
