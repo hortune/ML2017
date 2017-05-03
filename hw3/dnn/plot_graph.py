9c39e5e644b2bf35bfcfaede57ff968f270ce173
hortune <b04611015@ntu.edu.tw>
2017-05-03 04:04:10 -0700

    done

--- hw3/dnn/plot_graph.py
+++ hw3/dnn/plot_graph.py
@@ -22,15 +22,15 @@ ax1.set_title('Accuracy/Epoch')
 train_accur = [float(x[:-1]) for x in open('train_accuracy').readlines()]
 valid_accur = [float(x[:-1]) for x in open('valid_accuracy').readlines()]
 
-ax1.plot(list(range(1,21)),train_accur,color="red",label="train",lw=2)
-ax1.plot(list(range(1,21)),valid_accur,color="blue",label="validation",lw=2)
-ax1.legend(loc='lower right')
+ax1.plot(list(range(1,101)),train_accur,color="red",label="train",lw=2)
+ax1.plot(list(range(1,101)),valid_accur,color="blue",label="validation",lw=2)
+ax1.legend(loc='upper left')
 
 ax2.set_title('Loss/Epoch')
 train_accur = [float(x[:-1]) for x in open('train_loss').readlines()]
 valid_accur = [float(x[:-1]) for x in open('valid_loss').readlines()]
 
-ax2.plot(list(range(1,21)),train_accur,color="red",label="train",lw=2)
-ax2.plot(list(range(1,21)),valid_accur,color="blue",label="validation",lw=2)
-ax2.legend(loc='upper right')
+ax2.plot(list(range(1,101)),train_accur,color="red",label="train",lw=2)
+ax2.plot(list(range(1,101)),valid_accur,color="blue",label="validation",lw=2)
+ax2.legend(loc='upper left')
 fig.savefig('acccuracy_comp_dnn.png')
