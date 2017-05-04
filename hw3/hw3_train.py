fc32271f6393f4e227befc53dc3a25d8c37402e8
hortune <hortune@learnler.csie.ntu.edu.tw>
2017-05-04 21:53:24 +0800

    done

--- hw3/hw3_train.py
+++ hw3/hw3_train.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import sys
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.15
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 set_session(tf.Session(config=config))
 import numpy as np
 
