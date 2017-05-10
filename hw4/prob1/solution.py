e3a1c7f3bb08ab626b7d4d05431b7195a841e444
hortune <b04611015@ntu.edu.tw>
2017-05-10 02:12:49 -0700

    done

--- hw4/prob1/solution.py
+++ hw4/prob1/solution.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
@@ -15,4 +16,20 @@ mean = image_data.mean(axis=0)
 image_data -= mean
 
 covariance_matrix = np.cov(image_data.T)
-eigval,eigvec = np.linalg.eigh(covariance_matrix)
+eigval,eigvec = np.linalg.eigh(covariance_matrix) # eigh
+
+idx = eigval.argsort()[::-1]
+eigval = eigval[idx]
+eigvec = eigvec[:,idx]
+
+fig = plt.figure(figsize=(12,12))
+for i in range(9):
+    ax = fig.add_subplot(3,3,i+1)
+    ans = np.zeros(4096)
+    for q,p in zip(eigvec[i],image_data):
+        ans += q*p
+    ax.imshow(ans.reshape(64,64),cmap='gray',interpolation="nearest")
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    #plt.tight_layout()
+fig.savefig("solution.png")
