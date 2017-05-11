0abc9a093fcd32fa4a99598dd0e4ef977c0b7583
hortune <b04611015@ntu.edu.tw>
2017-05-10 20:01:10 -0700

    done

---
+++ hw4/prob1/question2.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+def load_data():
+    res_img = []
+    for i in range(ord('A'),ord('A')+10):
+        for j in range(10):
+            im = Image.open("dataset/"+chr(i)+"0"+str(j)+".bmp")
+            p = np.array(im).flatten()
+            res_img.append(p)
+    return np.array(res_img,dtype="float64")
+
+image_data = load_data()
+mean = image_data.mean(axis=0)
+image_data -= mean
+
+covariance_matrix = np.cov(image_data.T)
+eigval,eigvec = np.linalg.eigh(covariance_matrix) # eigh
+
+idx = eigval.argsort()[::-1]
+
+eigval = eigval[idx]
+eigvec = eigvec.T
+eigvec = eigvec[idx]
+
+
+
+fig = plt.figure(figsize=(12,12))
+for i in range(100):
+    ax = fig.add_subplot(10,10,i+1)
+    ax.imshow((image_data[i]).reshape(64,64),cmap='gray',interpolation="nearest")
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+fig.savefig("origin.png")
+
+
+
+res_image = []
+for row in eigvec[:5].dot(image_data.T).T:
+    #res = eigvec[:5].dot(image_data.T).T
+    res_image.append( (row*eigvec[:5].T).sum(axis=1).reshape(4096))
+
+
+fig = plt.figure(figsize=(12,12))
+for i in range(100):
+    ax = fig.add_subplot(10,10,i+1)
+    ax.imshow((res_image[i]).reshape(64,64),cmap='gray',interpolation="nearest")
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+fig.savefig("reconstruct.png")
--- hw4/prob1/question3.py
+++ hw4/prob1/question3.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
-
+from math import sqrt
 def load_data():
     res_img = []
     for i in range(ord('A'),ord('A')+10):
@@ -10,6 +10,11 @@ def load_data():
             p = np.array(im).flatten()
             res_img.append(p)
     return np.array(res_img,dtype="float64")
+def rmse(res,origin):
+    error = 0
+    for i,j in zip(res,origin):
+        error += (i-j).dot(i-j)
+    return sqrt(error/(100*4096))/255
 
 image_data = load_data()
 mean = image_data.mean(axis=0)
@@ -19,17 +24,13 @@ covariance_matrix = np.cov(image_data.T)
 eigval,eigvec = np.linalg.eigh(covariance_matrix) # eigh
 
 idx = eigval.argsort()[::-1]
-eigval = eigval[idx]
-eigvec = eigvec[:,idx]
 
-fig = plt.figure(figsize=(12,12))
-for i in range(9):
-    ax = fig.add_subplot(3,3,i+1)
-    ans = np.zeros(4096)
-    for q,p in zip(eigvec[i],image_data):
-        ans += q*p
-    ax.imshow(ans.reshape(64,64),cmap='gray',interpolation="nearest")
-    plt.xticks(np.array([]))
-    plt.yticks(np.array([]))
-    #plt.tight_layout()
-fig.savefig("solution.png")
+eigval = eigval[idx]
+eigvec = eigvec.T
+eigvec = eigvec[idx]
+for i in range(1,101):
+    res_image = []
+    for row in eigvec[:i].dot(image_data.T).T:
+        res_image.append( (row*eigvec[:i].T).sum(axis=1).reshape(4096))
+    res_image= np.array(res_image)
+    print ("error ",i,":",rmse(res_image,image_data))
