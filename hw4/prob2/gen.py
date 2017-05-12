62c499e22c581d96df228dbcf2e29ba05aa26a0b
hortune <b04611015@ntu.edu.tw>
2017-05-12 11:06:58 -0700

    hw4 p2 adjust_text

--- hw4/prob2/gen.py
+++ hw4/prob2/gen.py
@@ -3,6 +3,7 @@ import word2vec
 import nltk
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from adjustText import adjust_text
 
 model = word2vec.load('model.bin')
 words, vectors = model.vocab,model.vectors
@@ -18,13 +19,19 @@ k_vec = []
 prohibit=set("’|”“()‘`,.:;\'!?\"")
 allow = set(["JJ","NNP","NN","NNS"])
 #plt.scatter(trans_vec[:,0],trans_vec[:,1])
+
+fig = plt.figure(figsize=(20,12))
+texts = []
+
 for word,vec in zip(text[:500],trans_vec[:500]):
     #print( word[0],word[1])
     if set(word[0])&prohibit or word[1] not in allow:
         continue
     print( word[0],word[1])
-    plt.annotate(word[0],xy=vec,xytext=(0,0),textcoords='offset points')
+    #plt.annotate(word[0],xy=vec,xytext=(0,0),textcoords='offset points')
+    texts.append(plt.text(vec[0],vec[1],word[0]))
     k_vec.append(list(vec))
 
 plt.scatter(np.array(k_vec)[:,0],np.array(k_vec)[:,1])
-plt.show()
+adjust_text(texts,arrowprops=dict(arrowstyle="-",color='k',lw=0.5))
+fig.savefig('word2vec.png')
