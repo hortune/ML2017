454f84b70d18a2a75d300b2295de52fe5f3f6b8d
hortune chen <b04611015@ntu.edu.tw>
2017-05-24 19:58:54 +0000

    modify

--- hw5/54293/run.sh
+++ hw5/54293/run.sh
@@ -1,5 +1,6 @@
-python3 0.py train_data.csv  $1
-python3 1.py train_data.csv  $1
-python3 2.py test_data.csv
-python3 3.py 
+python3 preprocess.py > /dev/null
+python3 0.py train_data.csv  $1 > /dev/null
+python3 1.py train_data.csv  $1 > /dev/null
+python3 2.py $1 > /dev/null
+python3 3.py  > /dev/null
 python3 vote.py $2
